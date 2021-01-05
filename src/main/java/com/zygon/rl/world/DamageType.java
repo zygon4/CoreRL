4e48d316a28efe5f075fadffd40fe7f132a5b7c6
zygon4 <dcharubini@gmail.com>
2021-01-04 22:58:23 -0500

    Add Damage type enum

---
+++ src/main/java/com/zygon/rl/world/DamageType.java
@@ -0,0 +1,21 @@
+package com.zygon.rl.world;
+
+/**
+ *
+ * @author zygon
+ */
+public enum DamageType {
+    Slashing,
+    Piercing,
+    Bludgeoning,
+    Poison,
+    Acid,
+    Fire,
+    Cold,
+    Radiant,
+    Necrotic,
+    Lightning,
+    Thunder,
+    Force,
+    Psychic
+}
