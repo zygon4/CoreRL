3241a303cb8d299a68dd8d9be3b6d4c213e5cebd
zygon <>
2021-06-19 12:01:55 -0400

    Set volume (lower) by default.

--- src/main/java/com/zygon/rl/util/Audio.java
+++ src/main/java/com/zygon/rl/util/Audio.java
@@ -3,6 +3,7 @@ package com.zygon.rl.util;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
@@ -30,7 +31,17 @@ public class Audio {
     // Method to play the audio
     public void play() throws LineUnavailableException, IOException {
         clip.open(audioInputStream);
+        setVolume(0.10f);
         clip.loop(Clip.LOOP_CONTINUOUSLY);
         clip.start();
+    }
+
+    // https://stackoverflow.com/questions/40514910/set-volume-of-java-clip
+    public void setVolume(float volume) {
+        if (volume < 0f || volume > 1f) {
+            throw new IllegalArgumentException("Volume not valid: " + volume);
+        }
+        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl.setValue(20f * (float) Math.log10(volume));
     }
 }
