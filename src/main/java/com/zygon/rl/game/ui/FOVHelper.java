13295324505e9d4eff4f6d267bc94e82daeea6fe
David <dcharubini@gmail.com>
2022-01-02 01:14:56 -0500

    Items should view block

--- src/main/java/com/zygon/rl/game/ui/FOVHelper.java
+++ src/main/java/com/zygon/rl/game/ui/FOVHelper.java
@@ -1,8 +1,9 @@
 package com.zygon.rl.game.ui;
 
+import com.zygon.rl.data.Element;
 import com.zygon.rl.data.Identifable;
 import com.zygon.rl.data.Terrain;
-import com.zygon.rl.data.WorldElement;
+import com.zygon.rl.data.context.Data;
 import com.zygon.rl.world.CommonAttributes;
 import com.zygon.rl.world.Location;
 import com.zygon.rl.world.World;
@@ -26,13 +27,27 @@ class FOVHelper {
         float[][] portion = new float[maxValues.getX() - minValues.getX()][maxValues.getY() - minValues.getY()];
         for (int y = minValues.getY(); y < maxValues.getY(); y++) {
             for (int x = minValues.getX(); x < maxValues.getX(); x++) {
-                Terrain terrain = world.getTerrain(Location.create(x, y));
-                List<Identifable> allElements = world.getAllElements(Location.create(x, y));
 
-                // TODO:
-                double viewBlocking = getMaxViewBlock(terrain);
+                List<Identifable> allElements = world.getAllElements(Location.create(x, y));
+                // junk in the way
+                double stuffViewBlock = allElements.stream()
+                        .map(Identifable::getId)
+                        .filter(id -> !id.equals("player"))
+                        .map(id -> {
+                            Element element = Data.get(id);
+                            return element;
+                        })
+                        .map(FOVHelper::getMaxViewBlock)
+                        .mapToDouble(v -> v)
+                        .max().orElse(0);
+
+                Terrain terrain = world.getTerrain(Location.create(x, y));
+                double terrainViewBlocking = getMaxViewBlock(terrain);
+
+                double maxViewBlock = Math.max(stuffViewBlock, terrainViewBlocking);
+
                 try {
-                    portion[x - minValues.getX()][y - minValues.getY()] = (float) viewBlocking;
+                    portion[x - minValues.getX()][y - minValues.getY()] = (float) maxViewBlock;
                 } catch (java.lang.ArrayIndexOutOfBoundsException ex) {
                     throw new RuntimeException(ex);
                 }
@@ -41,7 +56,7 @@ class FOVHelper {
         return portion;
     }
 
-    public static double getMaxViewBlock(WorldElement entity) {
+    public static double getMaxViewBlock(Element entity) {
         Double viewBlocker = entity.getFlag(VIEW_BLOCK_NAME);
         return viewBlocker != null ? viewBlocker : 0.0;
     }
