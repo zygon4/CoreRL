c5b7f785a38e337502603af52804f842af9343e4
David <dcharubini@gmail.com>
2022-07-16 22:24:10 -0400

    Fix issue where player cannot see tile to the south of them due to shadowcasting.

--- src/main/java/com/zygon/rl/game/ui/LitMap2DImpl.java
+++ src/main/java/com/zygon/rl/game/ui/LitMap2DImpl.java
@@ -4,44 +4,44 @@ import com.stewsters.util.shadow.twoDimention.LitMap2d;
 
 final class LitMap2DImpl implements LitMap2d {
 
-    private final float[][] lightResistances;
-    private final float[][] light;
+  private final float[][] lightResistances;
+  private final float[][] light;
 
-    public LitMap2DImpl(float[][] lightResistances) {
-        this.lightResistances = lightResistances;
-        this.light = new float[this.lightResistances.length][this.lightResistances[0].length];
-    }
+  public LitMap2DImpl(float[][] lightResistances) {
+    this.lightResistances = lightResistances;
+    this.light = new float[this.lightResistances.length][this.lightResistances[0].length];
+  }
 
-    @Override
-    public void setLight(int startx, int starty, float force) {
-        // Inversed Y
-        this.light[startx][this.light[0].length - starty - 1] = force;
-    }
+  @Override
+  public void setLight(int startx, int starty, float force) {
+    // Inversed Y
+    this.light[startx][this.light[0].length - starty] = force;
+  }
 
-    @Override
-    public int getXSize() {
-        return lightResistances.length;
-    }
+  @Override
+  public int getXSize() {
+    return lightResistances.length;
+  }
 
-    @Override
-    public int getYSize() {
-        return lightResistances[0].length;
-    }
+  @Override
+  public int getYSize() {
+    return lightResistances[0].length;
+  }
 
-    @Override
-    public float getLight(int currentX, int currentY) {
-        return this.light[currentX][currentY];
-    }
+  @Override
+  public float getLight(int currentX, int currentY) {
+    return this.light[currentX][currentY];
+  }
 
-    @Override
-    public float getResistance(int currentX, int currentY) {
-        return lightResistances[currentX][currentY];
-    }
+  @Override
+  public float getResistance(int currentX, int currentY) {
+    return lightResistances[currentX][currentY];
+  }
 
-    @Override
-    public void addLight(int currentX, int currentY, float bright) {
-        // Inversed Y because the zircon screen's columns are bottom left, not top left
-        this.light[currentX][this.light[0].length - currentY] = bright;
-    }
+  @Override
+  public void addLight(int currentX, int currentY, float bright) {
+    // Inversed Y because the zircon screen's columns are bottom left, not top left
+    this.light[currentX][this.light[0].length - currentY] = bright;
+  }
 
 }
