6a2161e7abd6cdeb6a1e6fe15f05c530849e8604
zygon4 <zygon4@gmail.com>
2025-03-07 22:10:25 -0700

    Formatting

--- src/main/java/com/zygon/rl/util/Audio.java
+++ src/main/java/com/zygon/rl/util/Audio.java
@@ -15,40 +15,40 @@ import java.nio.file.Path;
  */
 public class Audio {
 
-  private final Path filePath;
+    private final Path filePath;
 
-  public Audio(Path filePath) {
-    this.filePath = filePath;
-  }
-
-  public void play(boolean loop) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
-    try ( AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(filePath.toFile())) {
-      Clip clip = AudioSystem.getClip();
-      clip.open(audioInputStream);
-      if (loop) {
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
-      }
-      setVolume(clip, 0.10f);
-      clip.start();
+    public Audio(Path filePath) {
+        this.filePath = filePath;
     }
-  }
 
-  public void play() throws LineUnavailableException, IOException, UnsupportedAudioFileException {
-    play(false);
-  }
-
-  public void playLoop() throws LineUnavailableException, IOException, UnsupportedAudioFileException {
-    play(true);
-  }
-
-  // https://stackoverflow.com/questions/40514910/set-volume-of-java-clip
-  public void setVolume(Clip clip, float volume) {
-    if (volume < 0f || volume > 1f) {
-      throw new IllegalArgumentException("Volume not valid: " + volume);
+    public void play(boolean loop) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(filePath.toFile())) {
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            if (loop) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+            setVolume(clip, 0.10f);
+            clip.start();
+        }
     }
-    if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
-      FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-      gainControl.setValue(20f * (float) Math.log10(volume));
+
+    public void play() throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        play(false);
     }
-  }
+
+    public void playLoop() throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        play(true);
+    }
+
+    // https://stackoverflow.com/questions/40514910/set-volume-of-java-clip
+    public void setVolume(Clip clip, float volume) {
+        if (volume < 0f || volume > 1f) {
+            throw new IllegalArgumentException("Volume not valid: " + volume);
+        }
+        if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(20f * (float) Math.log10(volume));
+        }
+    }
 }
